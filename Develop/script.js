d90916f95204f2a08765e8b051add416a4a8f5d0
Tina Humphrey <tinahumphrey@Tinas-MacBook-Air.local>
2023-11-05 22:50:06 -0500

    commit draft changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowercase = "abcdefghijklmnopqrstuvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXZ";
-var number = "0123456789";
-var specialcharacter ="!@#$%^&*()?.<\>|=+:;,[-_]";
-
-//WHEN I click the button to generate a password
-//THEN I am presented with a series of prompts for password criteria
-
-var passwordCriteria =  confirm("Your password should have an uppercase letter, lowercase letter, numeric, and a special character! Click OK to continue");
-
-var passwordCharacter = confirm("Your password have at least 8 characters and no more than 128 characters! Click OK to continue");
-
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-var userChoice = window.prompt("Select the criteria to include in the password.");
-
-function selectpassword(lowercase, uppercase, number, specialcharacter)
-{
-  var option = document.createElement("option");
-  option.lowercase = lowercase;
-  option.uppercase = uppercase;
-  option.number = number;
-  option.specialcharacter = specialcharacter;
-
-  // If user pressed Cancel, immediately end function
-  if (!userChoice) {
-  return;
-}
-}
-
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
+var passChoice = {
+ lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+ uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Z"],
+ number:["0","1","2","3","4","5","6","7","8","9"],
+ specialcharacter:["!","@","#","$","%","^","&","*","(",")","?",".","<","-"]
+};
 
 function generatePassword() {
   let password = '';
@@ -42,22 +15,11 @@ function generatePassword() {
       let char = Math.floor(Math.random() * str.length + 1);
 
       password += str.charAt(char)
-
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
   }
 
   return password;
-
 }
-if (passwordCriteria === password) {
-window.alert("Your password matches the password criteria");
-  generatePassword();
 
-} else {
-  window.alert("Your password does not match the password criteria");
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -65,7 +27,37 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+
+
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+var passwordUpper =  confirm("Do you want your password to include upper case letters? Click OK to continue");
+var passwordLower = confirm("Do you want your password to include lower case letters? Click OK to continue");
+var passwordNumber = confirm("Do you want your password to include numbers? Click OK to continue");
+var passwordSpecialCharacters = confirm("Do you want your password to include special characters? Click OK to continue");
+
+
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+var passwordCharacter = confirm("Your password have at least 8 characters and no more than 128 characters! Click OK to continue");
+var userChoice = window.prompt("How many characters do you want in your passwor?");
+
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+
+if (passChoice === password){
+  lowercase;
+}
+else if (
+  (passChoice === uppercase)  
+  (passChoice ===  number)
+  (passChoice === specialcharacter)
+) {
+
 }
 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

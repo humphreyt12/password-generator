bb08d30a5ebb0e7b57605d58115eefa78898a394
Tina Humphrey <tinahumphrey@Tinas-MacBook-Air.local>
2023-11-06 22:29:09 -0500

    commit final draft changes to project

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passChoice = {
+
+
+function generatePassword() {
+  let password = '';
+  let str = [];
+  var passChoice = {
  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Z"],
  number:["0","1","2","3","4","5","6","7","8","9"],
  specialcharacter:["!","@","#","$","%","^","&","*","(",")","?",".","<","-"]
 };
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+var passwordUpper =  confirm("Do you want your password to include upper case letters? Click OK to continue");
+var passwordLower = confirm("Do you want your password to include lower case letters? Click OK to continue");
+var passwordNumber = confirm("Do you want your password to include numbers? Click OK to continue");
+var passwordSpecialCharacters = confirm("Do you want your password to include special characters? Click OK to continue");
 
-function generatePassword() {
-  let password = '';
-  let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?.<\>|=+:;,[-_]';
 
-  for (let i = 0; i <= 8; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
+if (passwordUpper) {
+  str = str.concat(passChoice.uppercase);
+}
 
-      password += str.charAt(char)
+
+if (passwordLower) {
+  str = str.concat(passChoice.lowercase);
+}
+
+
+if (passwordNumber) {
+  str = str.concat(passChoice.number);
+}
+
+
+if (passwordSpecialCharacters) {
+  str = str.concat(passChoice.specialcharacter);
+}
+
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+var passwordCharacter = confirm("Your password have at least 8 characters and no more than 128 characters! Click OK to continue");
+var userChoice = window.prompt("How many characters do you want in your password?");
+console.log(userChoice);
+
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+
+if (userChoice < 8 || userChoice > 128) {
+ window.alert("Your password is not at least 8 characters and no more than 128 characters.");
+ return "Please try again!";
+
+} 
+
+
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+  
+for (let i = 0; i < userChoice; i++) {
+      let char = Math.floor(Math.random() * str.length);
+
+      password += str[char]
   }
 
   return password;
+
+
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -31,32 +81,6 @@ function writePassword() {
 //THEN I am presented with a series of prompts for password criteria
 
 
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-var passwordUpper =  confirm("Do you want your password to include upper case letters? Click OK to continue");
-var passwordLower = confirm("Do you want your password to include lower case letters? Click OK to continue");
-var passwordNumber = confirm("Do you want your password to include numbers? Click OK to continue");
-var passwordSpecialCharacters = confirm("Do you want your password to include special characters? Click OK to continue");
-
-
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-var passwordCharacter = confirm("Your password have at least 8 characters and no more than 128 characters! Click OK to continue");
-var userChoice = window.prompt("How many characters do you want in your passwor?");
-
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-
-if (passChoice === password){
-  lowercase;
-}
-else if (
-  (passChoice === uppercase)  
-  (passChoice ===  number)
-  (passChoice === specialcharacter)
-) {
-
-}
 
 }
 // Add event listener to generate button
